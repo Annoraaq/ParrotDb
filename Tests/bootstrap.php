56e413b0ef23e26b80d4f92ea18cb08187192382
Annoraaq <baum@bw-webgestaltung.de>
2015-12-31 13:50:36 +0100

    fixed phpunit bootstrap

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -9,7 +9,7 @@
 /**
  * @author J. Baum
  */
-require '../vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
 //$autoloader = new PAutoloader('ParrotDb');
 //$autoloader->register();
